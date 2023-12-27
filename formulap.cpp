f154036aa5c8a76508aa1d7d6b0874b825a3ab85
MarkChase3 <103606684+MarkChase3@users.noreply.github.com>
2023-12-27 16:00:54 -0300

    Update formulap.cpp

--- formulap.cpp
+++ formulap.cpp
@@ -5,6 +5,37 @@ using namespace std;
 int main(int argc, char** argv){
 	if(argc > 2){
 		string cmd = argv[1];
+		if(cmd == "open"){
+			system("curl https://raw.githubusercontent.com/MarkChase3/formulap/main/problems/list.txt > tmp/list");
+			ifstream in("tmp/list");
+			string str, b;
+			cout << "SEARCHING FOR DUPLICATE NAMES...\n";
+			do{
+				b = str;
+				in >> str;
+				if(str == argv[2]){
+					cout << "UNABLE TO OPEN RACE: NAME ALREADY CLAIMED\n";
+					return 0;
+				}
+			} while(str != b);
+			string statement;
+			ifstream statementf("problems/"+string(argv[2])+".md");
+			statementf >> statement;
+
+			string code;
+			ifstream codef("problems/"+string(argv[2])+".cpp");
+			codef >> code;
+			system((
+				"curl -X POST https://xrbhog4g8g.execute-api.eu-west-2.amazonaws.com/prod/prb0t -H 'cache-control: no-cache' -H 'content-type: application/json' "
+				"-d '{\"user\": \"MarkChase3\",\"repo\": \"formulap\",\"description\": \""+string(argv[2])+"\",\"title\": \"New Race: "+string(argv[2])+"\",\"commit\": \"race data\",\"files\": [{\"path\": \"problems/"+string(argv[2])+".md\", \"content\": \""+statement+"\"},{\"path\": \"problems/"+string(argv[2])+".cpp\", \"content\": \""+code+"\"}]}'"
+			).c_str());
+		}
+		if(cmd == "join"){
+			cout << "FETCHING THE REQUIRED RACE...\n";
+			system(("curl https://raw.githubusercontent.com/MarkChase3/formulap/main/problems/"+string(argv[2])+".md > problems/"+string(argv[2])+".md").c_str());
+			system(("curl https://raw.githubusercontent.com/MarkChase3/formulap/main/problems/"+string(argv[2])+".cpp > problems/"+string(argv[2])+".cpp").c_str());
+			cout << "RACE JOINED\n";
+		}
 		if(cmd == "drive"){
 			cout << "CACHING..." << endl;
 			system(("cp problems/"+string(argv[2])+".cpp tmp/checker.cpp").c_str());
@@ -13,7 +44,7 @@ int main(int argc, char** argv){
 			system("g++ -o tmp/checker tmp/checker.cpp");
 			system("g++ -o tmp/answer tmp/answer.cpp");
 			cout << "SETTING UP..." << endl;
-			FILE *f = fopen("p", "w");
+			FILE *f = fopen("tmp/p", "w");
 			fprintf(f, " ");
 			fclose(f);
 			f = fopen("tmp/p", "r");
@@ -46,13 +77,12 @@ int main(int argc, char** argv){
 				fgets(pc, 10, f);
 
 				if(prevp != string(pc) and j > 1)cout << "TEST " << j-1 << " PASSED..." << endl;
-
 				j++;
 			}
 			
 			fclose(f);
 			if(prevp == "0")cout << "TEST 0 FAILED\nWRONG ANSWER\n";
-			else if(prevp != "100")cout << "TEST " << j-2 << " FAILED"<< "\nPARCIAL\n" << out << str;
+			else if(prevp != "100")cout << "TEST " << j-2 << " FAILED"<< "\nPARCIAL\n" << out;
 			else cout << "ALL TESTS PASSED\nACCEPTED\n";
 		}
 	}
